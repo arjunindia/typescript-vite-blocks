80fed3f5cd588710a892445195a3a9b431c0fc50
arjunindia <37960163+arjunindia@users.noreply.github.com>
2023-03-23 23:27:02 +0530

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,7 @@ import { luaGenerator } from 'blockly/lua';
 import blockContents from './blockContents';
 const demoWorkSpace = Blockly.inject(
   document.querySelector('#blocklydiv') as HTMLDivElement,
-  {
-    media: 'https://blockly-demo.appspot.com/static/media/',
-    toolbox: {
-      kind: 'categoryToolbox',
-      contents: blockContents,
-    },
-    collapse: true,
-    comments: true,
-    disable: true,
-    maxBlocks: Infinity,
-    trashcan: true,
-    horizontalLayout: false,
-    toolboxPosition: 'start',
-    css: true,
-    rtl: false,
-    scrollbars: true,
-    sounds: true,
-    oneBasedIndex: true,
-    grid: {
-      spacing: 20,
-      length: 1,
-      colour: '#888',
-      snap: true,
-    },
-    zoom: {
-      controls: true,
-      wheel: true,
-      startScale: 1,
-      maxScale: 3,
-      minScale: 0.3,
-      scaleSpeed: 1.2,
-    },
-  }
+  blockContents
 );
 function runtest() {
   console.log(luaGenerator.workspaceToCode(demoWorkSpace));
